70a2ecbdef56511ca3dc6a40bc1ba3644a91ac0f
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-04-15 19:28:13 +0200

    Create Room model with title, description, price, and location fields.

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,3 +5,12 @@ class Message(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Room(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField()
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
