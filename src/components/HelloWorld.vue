f7661a89e366612950cb53eb45b7dfae502f448a
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-04-17 11:35:15 +0200

    Add all rooms and responsive Bootstrap card styles for room lists

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,21 @@
 <template>
-  <div>
-      <h1>Room Details</h1>
-      <div v-if="room">
-          <p>Title: {{ room.title }}</p>
-          <p>Description: {{ room.description }}</p>
-          <p>Price: {{ room.price }}</p>
-          <p>Location: {{ room.location }}</p>
+  <div class="container">
+    <h1>Available Rooms</h1>
+    <div class="row">
+      <div class="col-md-4" v-for="room in rooms" :key="room.id">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><b>{{ room.title }}</b></h5>
+            <p class="card-text">{{ room.description }}</p>
+            <p class="card-text">Price: CHF {{ room.price }}.-</p>
+            <p class="card-text">Location: {{ room.location }}</p>
+          </div>
+        </div>
       </div>
-      <div v-else>
-          <p>There is no room available.</p>
+      <div class="col-12" v-if="rooms.length === 0">
+        <p>No rooms available at the moment.</p>
       </div>
+    </div>
   </div>
 </template>
 
@@ -18,17 +24,17 @@ import axios from '@/services/api';
 export default {
   name: "HelloWorld",
   data() {
-        return {
-            room: null
-        };
-    },
+    return {
+      rooms: [],
+    };
+  },
   props: {
     msg: String
   },
   mounted() {
-      axios.get('/rooms/1/')
+      axios.get('/rooms/')
           .then(response => {
-              this.room = response.data;
+              this.rooms = response.data;
           })
           .catch(error => {
               console.error('There was an error!', error);
@@ -39,6 +45,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css';
+
+.card {
+  margin-bottom: 1rem;
+  border-radius: 8px;
+}
+
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
@@ -58,6 +71,12 @@ h3 {
   .greetings h1,
   .greetings h3 {
     text-align: left;
+  }
+}
+
+@media (max-width: 768px) {
+  .card-img-top {
+    height: 40vw;
   }
 }
 </style>
