62b92c2a27e3568e96c3bab81c260f5d3e866245
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-04-21 20:38:12 +0200

    Integrate Quasar framework for enhanced UI components and responsiveness

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
+import { Quasar } from 'quasar';
 
 const app = createApp(App)
+
+app.use(Quasar, {
+    // Quasar plugins go here
+  });
+
 app.use(router)
 app.mount("#app")
