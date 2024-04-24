2387e25bc4be40fc5234e69c6778370073b45ec7
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-04-24 21:57:15 +0200

    Defined more routing paths

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ const routes = [
     children: [
       { path: '', name: 'home', component: () => import('@/views/HomeView.vue') },
       { path: '/messages', name: 'messages', component: () => import(/* webpackChunkName: "about" */'@/views/MessagesView.vue') },
+      { path: '/room/:id', name: 'room', component: () => import('@/views/RoomView.vue') },
+      { path: '/:pathMatch(.*)*', name: 'not-found', component: () => import('@/views/404View.vue') }
     ]
   },
 ]
