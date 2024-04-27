d191a9c9744d650c34b8c7b6337961f85fb6c430
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-04-27 13:54:11 +0200

    Removed RoomInformation since RoomDetails is already used

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,10 @@ const routes = [
     component: MainLayout,
     children: [
       { path: '', name: 'home', component: () => import('@/views/HomeView.vue') },
+      { path: '/profile', name: 'Profile', component: () => import('@/views/ProfileView.vue')},
       { path: '/messages', name: 'messages', component: () => import(/* webpackChunkName: "about" */'@/views/MessagesView.vue') },
       { path: '/room/:id', name: 'room', component: () => import('@/views/RoomView.vue') },
-      { path: '/:pathMatch(.*)*', name: 'not-found', component: () => import('@/views/404View.vue') },
-      { path: '/room-information/:id', name: 'RoomInformation', component: () => import('@/components/RoomInformation.vue')}
+      { path: '/:pathMatch(.*)*', name: 'not-found', component: () => import('@/views/404View.vue') }
     ]
   },
 ]
