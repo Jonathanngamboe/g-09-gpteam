e11a4b9baff128cad1f1d61000668f891dee37fa
$Francesco <francesco.defino@students.hevs.ch>
2024-04-28 20:05:43 +0200

    fix server error

--- src/router/index.js
+++ src/router/index.js
@@ -21,12 +21,13 @@ const routes = [
       { path: '/messages', name: 'messages', component: () => import(/* webpackChunkName: "about" */'@/views/MessagesView.vue') },
       { path: '/room/:id', name: 'room', component: () => import('@/views/RoomView.vue') },
       { path: "/Add-Room", name: "Add-Room", component: AddRoomView },
+      { path: "/UserAccount", name: "UserAccount", component: UserAccountView },
       { path: '/:pathMatch(.*)*', name: 'not-found', component: () => import('@/views/404View.vue') },
       
       
     ]
   },
-  { path: "/UserAccount", name: "UserAccount", component: UserAccountView },
+  
   
   
   
