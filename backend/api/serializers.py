fad8bf062386c58880f6a4dc527c0141b9de2109
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-04-30 02:07:00 +0200

    Added amenities to the property

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -23,13 +23,19 @@ class CitySerializer(serializers.HyperlinkedModelSerializer):
         model = City
         fields = ['url', 'name', 'zip']
 
+class AmenitySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Amenity
+        fields = ['url', 'name']
+
 class PropertySerializer(serializers.HyperlinkedModelSerializer):
     images = ImageSerializer(many=True, read_only=True)
     average_rating = serializers.SerializerMethodField()
     city = CitySerializer(read_only=True)
+    amenities = AmenitySerializer(many=True, read_only=True)
     class Meta:
         model = Property
-        fields = ['id', 'url', 'title', 'description', 'address', 'city', 'price_per_night', 'surface', 'is_active', 'created_at', 'updated_at', 'owner', 'images', 'average_rating']
+        fields = ['id', 'url', 'title', 'description', 'address', 'city', 'price_per_night', 'surface', 'is_active', 'created_at', 'updated_at', 'owner', 'images', 'average_rating', 'amenities']
 
     def get_average_rating(self, obj):
         average = Review.objects.filter(property=obj).aggregate(Avg('rating'))
@@ -38,11 +44,6 @@ class PropertySerializer(serializers.HyperlinkedModelSerializer):
 class Property_TypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Property_Type
-        fields = ['url', 'name']
-
-class AmenitySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Amenity
         fields = ['url', 'name']
 
 class StatusSerializer(serializers.HyperlinkedModelSerializer):
