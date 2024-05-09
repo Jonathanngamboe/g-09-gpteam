4f03c9d37ed5774243a370027e265ed145b51b7f
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-05-09 19:20:33 +0200

    Added back createWebHashHistory to resolve the problem of pages not being found in the main branch

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 import MainLayout from "@/layouts/MainLayout.vue"
 import UserAccountView from '@/views/UserAccountView.vue'
 import AddPropertyView from '@/views/AddPropertyView.vue'
@@ -39,7 +39,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
