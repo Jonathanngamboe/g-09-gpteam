b3193b48b1609eb3fbe14ef08dc614cf973fcf3c
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-05-11 20:29:48 +0200

    Configured local SMTP and email backend settings for development testing

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -96,6 +96,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
