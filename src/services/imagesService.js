f0a35c49a9952bea4f47d8817fe2dc0b0a433c9b
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-05-24 03:03:20 +0200

    Added function to add image to property

--- src/services/imagesService.js
+++ src/services/imagesService.js
@@ -1,10 +1,11 @@
 import api from './api';
 
 const endpoint = '/images';
+const propertiesEndpoint = '/properties';
 
 export default {
     getAllImages() {
-        return api.get(endpoint)
+        return api.get(`${endpoint}/`)
             .then(response => response.data)
             .catch(error => {
                 throw new Error('Failed to fetch images: ' + error.message);
@@ -20,7 +21,8 @@ export default {
     },
     
     createImage(imageData) {
-        return api.post(endpoint, imageData)
+        const config = imageData instanceof FormData ? { headers: { 'Content-Type': 'multipart/form-data' } } : {};
+        return api.post(`${endpoint}/`, imageData, config)
             .then(response => response.data)
             .catch(error => {
                 throw new Error('Failed to create image: ' + error.message);
@@ -41,5 +43,20 @@ export default {
             .catch(error => {
                 throw new Error(`Failed to delete image with ID ${id}: ` + error.message);
             });
+    },
+
+    uploadImageForProperty(propertyId, imageData) {
+        return this.createImage(imageData)
+            .then(image => {
+                return api.post(`${propertiesEndpoint}/${propertyId}/add-image/`, { imageId: image.id })
+                    .then(response => response.data)
+                    .catch(error => {
+                        throw new Error(`Failed to upload image for property with ID ${propertyId}: ` + error.message);
+                    });
+            })
+            .catch(error => {
+                throw new Error('Failed to create image: ' + error.message);
+            });
     }
+    
 };
