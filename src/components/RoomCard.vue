96f4f01c598093e67d15f73bd5f8f70797a097e5
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-06-05 12:10:36 +0200

    Fixed image display

--- src/components/RoomCard.vue
+++ src/components/RoomCard.vue
@@ -2,7 +2,7 @@
     <q-card flat bordered>
       <!-- Room image -->
       <img
-        :src="room.images && room.images.length > 0 ? (room.images[0].image || room.images[0].ext_url) : 'https://www.trojanpress.com.au/wp-content/uploads/2019/05/Image-Coming-Soon-02-1024x683.jpg'"
+        :src="room.images && room.images.length > 0 ? (room.images_url || room.images[0].ext_url) : 'https://www.trojanpress.com.au/wp-content/uploads/2019/05/Image-Coming-Soon-02-1024x683.jpg'"
         alt="Room image"
       />
 
