e0df204c8d02944d1d5cef511095865cdabe9da6
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-06-05 14:36:14 +0200

    Added a 404 page

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -1,1 +1,34 @@
-<template></template>
+<template>
+    <q-page class="flex flex-center">
+        <div class="text-center q-gutter-md">
+            <h1>404</h1>
+            <h2>Page not found</h2>
+            <q-btn
+                unelevated
+                rounded
+                color="primary"
+                label="Go back"
+                @click="goBack"
+            />
+        </div>
+    </q-page>
+</template>
+
+<script>
+    import { defineComponent } from 'vue';
+    import { useRouter } from 'vue-router';
+
+    export default defineComponent({
+        setup() {
+            const router = useRouter();
+
+            const goBack = () => {
+                router.go(-1);
+            };
+
+            return {
+                goBack
+            };
+        }
+    });
+</script>
