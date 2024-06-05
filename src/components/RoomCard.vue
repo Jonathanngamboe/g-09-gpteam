ac8d32c45dc520369b2ee080bc7b81296af2772b
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-06-05 19:49:06 +0200

    Fixed local images display

--- src/components/RoomCard.vue
+++ src/components/RoomCard.vue
@@ -2,8 +2,8 @@
     <q-card flat bordered>
       <!-- Room image -->
       <img
-        :src="room.images && room.images.length > 0 ? (room.images_url || room.images[0].ext_url) : 'https://www.trojanpress.com.au/wp-content/uploads/2019/05/Image-Coming-Soon-02-1024x683.jpg'"
-        alt="Room image"
+        :src="room.images && room.images.length > 0 ? (room.images[0].image_url || room.images[0].ext_url) : 'https://www.trojanpress.com.au/wp-content/uploads/2019/05/Image-Coming-Soon-02-1024x683.jpg'"
+        alt="Room image" style="height: 15em;"
       />
 
       <!-- Card content -->
